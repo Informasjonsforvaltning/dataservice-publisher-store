571aad5a7bb8169e3536a49f7d228d257c224f09
valosnah <h.o.sund@gmail.com>
2022-09-19 11:55:01 +0200

    chore: add listener for compactAction, updates

---
+++ src/main/java/no/fdk/rabbit/RabbitMQConfig.java
@@ -0,0 +1,17 @@
+package no.fdk.rabbit;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.amqp.core.*;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@RequiredArgsConstructor
+public class RabbitMQConfig {
+
+    @Bean
+    public Queue compactQueue() {
+        return new AnonymousQueue();
+    }
+
+}
---
+++ src/main/java/no/fdk/rabbit/RabbitMQListener.java
@@ -0,0 +1,30 @@
+package no.fdk.rabbit;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import no.fdk.configuration.FusekiConfiguration;
+import no.fdk.model.fuseki.action.CompactAction;
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class RabbitMQListener {
+    private final CompactAction compactAction;
+    private final FusekiConfiguration fusekiConfiguration;
+
+    @RabbitListener(queues = "#{compactQueue.name}")
+    public void receiveConceptPublisher(Message message) {
+        String routingKey = message.getMessageProperties().getReceivedRoutingKey();
+
+        log.info("Received message from compact exchange with key: {}'", routingKey);
+
+        fusekiConfiguration.getDatasetNames()
+                .stream()
+                .map(dataset -> "%s/%s".formatted(fusekiConfiguration.getStorePath(), dataset))
+                .forEach(compactAction::compact);
+    }
+
+}
